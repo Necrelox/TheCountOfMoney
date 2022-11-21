17cfca407877dda842d69aa439fc15bd0f1ec816
Ruby <nicolas.da-silva@epitech.eu>
2022-11-21 10:28:48 +0100

    {+} Add builder and knex config for migration and seeds

---
+++ Api/repos/knexfile.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+
+/**
+ * @type { Object.<string, import('knex').Knex.Config> }
+ */
+module.exports = {
+    development: {
+        client: 'mysql2',
+        connection: {
+            host: '127.0.0.1',
+            user: 'ruby',
+            password: 'gw86!e7h',
+            database: 'EchBoard',
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: 'MIGRATION',
+        }
+    },
+
+    staging: {
+        client: 'mysql2',
+        connection: {
+            database: 'my_db',
+            user: 'username',
+            password: 'password'
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: 'MIGRATION'
+        }
+    },
+
+    production: {
+        client: 'mysql2',
+        connection: {
+            database: 'my_db',
+            user: 'username',
+            password: 'password'
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: 'MIGRATION'
+        }
+    }
+
+};
